736eb90a2f7354b32bd6414df940ba9eb3076cd1
Cecilia (Chae Hyun) Lee <chaelee@email.unc.edu>
2022-02-21 16:43:33 -0500

    flip

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
+import { coinFlip, coinFlips, countFlips, flipACoin } from "./coin.mjs";
 // Require Express.js
 const express = require('express')
 const app = express()
+const statusCode = 200;
+const status = "OK";
 const args = minimist(process.argv.slice(2))
 args["port"]
 const port = args.port || 5000;
@@ -16,11 +19,19 @@ app.use(function (req, res) {
     res.status(404).send('404 NOT FOUND')
 });
 
-app.get('/app/', (req, res) => {
+app.get('/app', (req, res) => {
     // Respond with status 200
-    res.statusCode = 200;
+    res.statusCode = statusCode;
     // Respond with status message "OK"
-    res.statusMessage = 'OK';
-    res.writeHead(res.statusCode, { 'Content-Type': 'text/plain' });
-    res.end(res.statusCode + ' ' + res.statusMessage)
+    res.statusMessage = status;
+    res.writeHead(res.statusCode, { "Content-Type": "text/plain" });
+    res.end(res.statusCode + " " + res.statusMessage);
 });
+
+app.get("/app/flip", (req, res) => {
+    let flipRes = coinFlip()
+    res.status(status).json({
+        'flip': flipRes
+    })
+});
+
