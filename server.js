73a1466637eb6065d008f0db23829d40903459d2
Cecilia (Chae Hyun) Lee <chaelee@email.unc.edu>
2022-02-24 21:15:16 -0500

    YAY? YAY?

--- server.js
+++ server.js
@@ -63,13 +63,19 @@ function flipACoin(call) {
 }
 // end coin functions
 
-app.get('/app/', (req, res) => {
-    res.statusCode = 200
-    res.statusMessage = 'OK'
-    res.writeHead(res.statusCode, { "Content-Type": CONTENT_TYPE_TEXT_PLAIN });
-    res.end(res.statusCode + " " + res.statusMessage);
-})
+app.get("/app/", (req, res) => {
+  res.statusCode = 200;
+  res.statusMessage = "OK";
+  res.writeHead(res.statusCode, { "Content-Type": "text/plain" });
+  res.end(res.statusCode + " " + res.statusMessage);
+});
 
 const server = app.listen(port, () => {
-  console.log("App listening on port %PORT%".replace("%PORT", port));
+  console.log("App listening on port %PORT%".replace("%PORT%", aPort));
+});
+
+
+// Default response for any other request
+app.use(function(req, res){
+    res.status(404).send('404 NOT FOUND')
 });
